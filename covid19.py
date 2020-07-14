17b6982a0b65f23c5fe3cf3d1c8c66c10b75f6d7
edwardwawrzynek <edward@wawrzynek.com>
2020-07-13 23:13:09 -0600

    misc improvements

--- covid19.py
+++ covid19.py
@@ -5,67 +5,77 @@ import matplotlib.pyplot as plt
 import sys
 
 def usage():
-	print("Usage: covid19.py path/to/covid-19-data [options]... [area]...\narea: \"US\" | state | county,state\noptions:\n\t--log\t:logarithmic y axis\n\t--diff\t:plot 1st derivatives of cases")
+	print(
+"""\
+USAGE: covid19.py path/to/covid-19-data [OPTIONS]... [LOCATIONS]...
+LOCATIONS:
+  "US" | state | county,state
+OPTIONS:
+  --log       : use a logarithmic y-axis (default is linear)
+  --deaths    : graph number of deaths (default is number of cases)
+  --diff      : graph the first derivative of cases/deaths in addition to just cases/deaths
+  --diff-only : only graph the first derivative of cases/deaths
+
+EXAMPLE:
+  covid19.py path/to/covid-19-data --log --deaths --diff-only "US" "New York" "Boulder,Colorado"
+    Graph the first derivative of the number of COVID deaths, on a logarithmic scale, in the US, the state of New York, and Boulder County
+""")
 	exit(1)
 
 graphAx = None
 doLog = False
+
+doTotal = True
 doDiff = False
+
+selector = "cases"
+
 
 def drawState(data, state):
 	global graphAx
-	state_only = (data.loc[data['state'] == state])[['date', 'cases']]
+	state_only = (data.loc[data['state'] == state])[['date', selector]]
 	state_only['date'] = pd.to_datetime(state_only['date'], format="%Y-%m-%d")
 	state_only = state_only.set_index('date')
 	state_diff = state_only.diff()
-	state_only = state_only.rename(columns={'cases': f'{state}'})
-	state_diff = state_diff.rename(columns={'cases': f'(d/dt) {state}'})
+	state_only = state_only.rename(columns={selector: f'{state}'})
+	state_diff = state_diff.rename(columns={selector: f'(d/dt) {state}'})
 	
-	if graphAx == None:
-		graphAx = state_only.plot(logy = doLog)
-	else:
-		state_only.plot(ax = graphAx, logy = doLog)
-	
+	if doTotal:
+		graphAx = state_only.plot(ax = graphAx, logy = doLog)
 	if doDiff:
-		state_diff.plot(ax = graphAx, logy=doLog)
+		graphAx = state_diff.plot(ax = graphAx, logy=doLog)
 
 def drawCounty(data, state, county):
 	global graphAx
-	county_only = ((data.loc[data['state'] == state]).loc[data['county'] == county])[['date', 'cases']]
+	county_only = ((data.loc[data['state'] == state]).loc[data['county'] == county])[['date', selector]]
 	county_only['date'] = pd.to_datetime(county_only['date'], format="%Y-%m-%d")
 	county_only = county_only.set_index('date')
 	county_diff = county_only.diff()
-	county_only = county_only.rename(columns={'cases': f'{county}, {state}'})
-	county_diff = county_diff.rename(columns={'cases': f'(d/dt) {county}, {state}'})
+	county_only = county_only.rename(columns={selector: f'{county}, {state}'})
+	county_diff = county_diff.rename(columns={selector: f'(d/dt) {county}, {state}'})
 	
-	if graphAx == None:
-		graphAx = county_only.plot(logy = doLog)
-	else:
-		county_only.plot(ax = graphAx, logy = doLog)
-	
+	if doTotal:
+		graphAx = county_only.plot(ax = graphAx, logy = doLog)
 	if doDiff:
-		county_diff.plot(ax = graphAx, logy = doLog)
+		graphAx = county_diff.plot(ax = graphAx, logy = doLog)
 
 def drawUS(data):
 	global graphAx
-	data = data[['date', 'cases']]
-	data = data.groupby('date')['cases'].sum().reset_index()
+	data = data[['date', selector]]
+	data = data.groupby('date')[selector].sum().reset_index()
 	data['date'] = pd.to_datetime(data['date'], format="%Y-%m-%d")
 	data = data.set_index('date')
 	diff = data.diff()
-	data = data.rename(columns={'cases': 'United States'})
-	diff = diff.rename(columns={'cases': '(d/dt) United States'})
+	data = data.rename(columns={selector: 'United States'})
+	diff = diff.rename(columns={selector: '(d/dt) United States'})
 
-	if graphAx == None:
-		graphAx = data.plot(logy = doLog)
-	else:
-		data.plot(ax = graphAx, logy = doLog)
-	
+	if doTotal:
+		graphAx = data.plot(ax = graphAx, logy = doLog)
 	if doDiff:
-		diff.plot(ax = graphAx, logy = doLog)
+		graphAx = diff.plot(ax = graphAx, logy = doLog)
 
 def main():
-	global doLog, doDiff
+	global doLog, doDiff, doTotal, selector
 	if(len(sys.argv) < 2):
 		usage()
 	state_file = sys.argv[1] + "/us-states.csv"
@@ -79,6 +89,11 @@ def main():
 				doLog = True
 			elif area[0] == '--diff':
 				doDiff = True
+			elif area[0] == '--deaths':
+				selector = "deaths"
+			elif area[0] == '--diff-only':
+				doTotal = False
+				doDiff = True
 			else:
 				usage()
 		elif area[0] == 'US':
@@ -89,7 +104,9 @@ def main():
 			drawCounty(county_data, area[1], area[0])
 		else:
 			usage()
-	
+
+		
+	plt.grid()
 	plt.show()
 
 main()
